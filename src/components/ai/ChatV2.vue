31e22a983e552a041d64748c30a5d60116a9270b
bot08 <71845954+bot08@users.noreply.github.com>
2025-09-03 16:29:56 +0300

    typo

--- src/components/ai/ChatV2.vue
+++ src/components/ai/ChatV2.vue
@@ -139,7 +139,7 @@
               <button v-if="comment.length < 6 || this.comment.length > 150" class="w-full py-2 px-4 text-base font-medium rounded-md text-white transition ease-in-out bg-indigo-500 dark:bg-indigo-700" style="cursor: not-allowed">Відправити</button>
               <div v-else>
                 <button v-if="isSendBtn" class="w-full py-2 px-4 text-base font-medium rounded-md text-white transition ease-in-out bg-indigo-500 dark:bg-indigo-700"><svg class="inline animate-spin -ml-1 mr-2 mb-0.5 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"> <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle> <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg> Відправка...</button>
-                <button v-else @click="sendComment()" class="w-full py-2 px-4 text-base font-medium rounded-md text-white transition ease-in-out bg-indigo-600 hover:bg-indigo-500 dark:hover:bg-indigo-700 focus:outline-none">Відправитиь</button>
+                <button v-else @click="sendComment()" class="w-full py-2 px-4 text-base font-medium rounded-md text-white transition ease-in-out bg-indigo-600 hover:bg-indigo-500 dark:hover:bg-indigo-700 focus:outline-none">Відправити</button>
               </div>
             </div>
           </div>
